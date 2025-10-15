568435e4144be625ac475d2c8793350b1a63417e
MarcPickel <marcpc@att.net>
2025-10-15 18:31:18 -0500

    Update User Schema to include email and password

--- models/user.js
+++ models/user.js
@@ -5,9 +5,18 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    unique: true,
     minlength: 2,
     maxlength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
   },
   avatar: {
     type: String,
